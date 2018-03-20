e65e55e41e791485ca556a54536166adee3a69a9
Glenn Yu <glennyu@stanford.edu>
2018-03-20 14:50:29 -0700

    update lstm models, ready for training

--- model/evaluation.py
+++ model/evaluation.py
@@ -1,6 +1,7 @@
 """Tensorflow utility functions for evaluation"""
 
 import logging
+import numpy as np
 import os
 
 from tqdm import trange
@@ -21,6 +22,7 @@ def evaluate_sess(sess, model_spec, num_steps, epoch, model_dir, writer=None, pa
     """
     profit = model_spec['profit']
     predictions = model_spec['predictions']
+    labels = model_spec['labels']
     update_metrics = model_spec['update_metrics']
     eval_metrics = model_spec['metrics']
     global_step = tf.train.get_global_step()
@@ -29,12 +31,17 @@ def evaluate_sess(sess, model_spec, num_steps, epoch, model_dir, writer=None, pa
     sess.run(model_spec['iterator_init_op'], feed_dict={model_spec['seed']: epoch})
     sess.run(model_spec['metrics_init_op'])
 
+    # Generate confusion matrix
+    conf_matrix = np.zeros((2, 2), dtype=np.int32)
+
     # compute metrics over the dataset
     #total_profit = 1.0
     all_preds = []
     for _ in range(num_steps):
-        _, profit_val, preds = sess.run([update_metrics, profit, predictions], feed_dict={model_spec['is_training']: True})
+        _, profit_val, preds, label_vals = sess.run([update_metrics, profit, predictions, labels], feed_dict={model_spec['is_training']: True})
         #total_profit *= (1 + profit_val)
+        for j in range(len(preds)):
+            conf_matrix[preds[j][0]][int(label_vals[j])] += 1
         all_preds.append(preds)
 
     eval_preds_file = os.path.join(model_dir, 'eval_preds.txt')
@@ -60,7 +67,7 @@ def evaluate_sess(sess, model_spec, num_steps, epoch, model_dir, writer=None, pa
             summ = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=val)])
             writer.add_summary(summ, global_step_val)
 
-    return metrics_val
+    return metrics_val, conf_matrix
 
 
 def evaluate(model_spec, model_dir, params, restore_from):
@@ -88,7 +95,7 @@ def evaluate(model_spec, model_dir, params, restore_from):
 
         # Evaluate
         num_steps = (params.eval_size + params.batch_size - 1) // params.batch_size
-        metrics = evaluate_sess(sess, model_spec, num_steps, model_dir)
+        metrics, _ = evaluate_sess(sess, model_spec, num_steps, model_dir)
         metrics_name = '_'.join(restore_from.split('/'))
         save_path = os.path.join(model_dir, "metrics_test_{}.json".format(metrics_name))
         save_dict_to_json(metrics, save_path)
