077d948854cd6fbb387e657783d1d40beca51ef5
Glenn Yu <glennyu@stanford.edu>
2018-03-20 11:15:25 -0700

    add baseline models for classification

---
+++ baseline_models/classify.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+import csv
+import numpy as np
+import pickle
+from sklearn.linear_model import RidgeClassifier as RC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+
+DIR = '../data/'
+train_prices_path = DIR + 'train_prices.txt'
+train_deltas_path = DIR + 'train_deltas.txt'
+val_prices_path = DIR + 'val_prices.txt'
+val_deltas_path = DIR + 'val_deltas.txt'
+
+NUM_FEATURES = 1
+
+# Return 3-d np array of inputs and 1-d np array of labels
+def get_inputs_and_labels():
+    return pickle.load(train_prices_path), pickle.load(train_deltas_path), 
+            pickle.load(val_prices_path), pickle.load(val_deltas_path)
+
+# Return trained RF classifier
+def random_forest_classifier(features, target):
+    clf = RandomForestClassifier()
+    clf.fit(features, target)
+    return clf
+
+# Return trained ridge classifier
+def ridge_classifier(X_train, y_train):
+    clf = RC()
+    clf.fit(X_train, y_train)
+    return clf
+
+# Return trained gradient boosting classifier
+def gradient_boosting_classifier(features, target):
+    clf = GradientBoostingClassifier(n_estimators=200, max_depth=4)
+    clf.fit(features, target)
+    return clf
+
+# Output training and validation accuracies 
+def train_model(X_train, y_train, X_val, y_val):
+    if model == "Random Forest":
+        trained_model = random_forest_classifier(X_train, y_train)
+        predictions = trained_model.predict(X_val)
+        mean_train_acc = accuracy_score(y_train, trained_model.predict(X_train))
+        mean_val_acc = accuracy_score(y_val, predictions)
+    elif model == "Ridge Classifier":
+        train_model = ridge_classifier(X_train, y_train)
+        mean_train_acc = clf.score(X_train, y_train)
+        mean_val_acc = clf.score(X_val, y_val)
+    elif model == "Gradient Boosting":
+        trained_model = gradient_boosting_classifier(X_train, y_train)
+        predictions = trained_model.predict(X_val)
+        mean_train_acc = accuracy_score(y_train, trained_model.predict(X_train))
+        mean_val_acc = accuracy_score(y_val, predictions)
+    else:
+        print "Model not found"
+    print 'train acc: %f, val acc: %f' % (mean_train_acc, mean_val_acc)
+
+def main():
+    X_train, y_train, X_val, y_val = get_inputs_and_labels()
+    print 'Predicting stock trends...'
+    train_model(X_train, y_train, X_val, y_val)
+
+if __name__ == '__main__':
+    main()
+
+
+            
