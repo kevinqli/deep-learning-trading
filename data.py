819503473261236f0f3d905f1cbbb6d50a0cd9cd
Kevin Q Li <likevinq@gmail.com>
2018-03-20 11:47:31 -0700

    technical indicators

--- data.py
+++ data.py
@@ -1,17 +1,68 @@
 from alpha_vantage.timeseries import TimeSeries
 import csv
-import cPickle as pickle
-import matplotlib.pyplot as plt
+import pickle
 import numpy as np
 import os
-from pprint import pprint
+import ta
+
+
+def MA5(df):
+    return ta.MA(df, 5)
+
+
+def MA10(df):
+    return ta.MA(df, 10)
+
+
+def EMA20(df):
+    return ta.EMA(df, 20)
+
+
+def ROC(df):
+    return ta.ROC(df, 5)
+
+
+def ATR(df):
+    return ta.ATR(df, 10)
+
+
+def BBANDS(df):
+    return ta.BBANDS(df, 20)
+
+
+def ADX(df):
+    return ta.ADX(df, 10, 5)
+
+
+def MACD(df):
+    return ta.MACD(df, 5, 20)
+
+
+def RSI(df):
+    return ta.RSI(df, 10)
+
+
+def MFI(df):
+    return ta.MFI(df, 10)
+
+
+def CCI(df):
+    return ta.CCI(df, 10)
+
+
+def KELCH(df):
+    return ta.KELCH(df, 20)
+
+
+indicators = [MA5, MA10, EMA20, ROC, ATR, BBANDS, ta.PPSR, ta.STOK,
+              ADX, MACD, RSI, MFI, CCI, KELCH]
 
 API_KEY = 'K4GGGZOT5MLPQ97T'
 
 SYM_FILE = 'companylist.csv'
 RAW_DATA_DIR = 'raw_data'
 TRAIN_DATA_DIR = 'data'
-WINDOW_SIZE = 30
+WINDOW_SIZE = 60
 
 TS = TimeSeries(key=API_KEY, output_format='pandas')
 
@@ -31,86 +82,87 @@ def load_sym(sym):
         return pickle.load(f)
 
 
-def split(prices, window=WINDOW_SIZE):
-    n = len(prices)
+def split(inputs, window=WINDOW_SIZE):
+    n = len(inputs)
     indices = np.arange(window, n, window)
-    inputs = np.split(prices[:indices[-2]], indices[:-2])
-    deltas = prices[indices[1:]] > prices[indices[:-1]]
-    assert len(inputs) == len(deltas)
-    return inputs, deltas
+    split_inputs = np.split(inputs[:indices[-2]], indices[:-2])
+    labels = inputs['Low'].ix[indices[1:]].as_matrix()\
+        > inputs['High'].ix[indices[:-1]].as_matrix()
+    assert len(split_inputs) == len(labels)
+    return split_inputs, labels
 
 
-def split_all_data_and_save(window=WINDOW_SIZE, label='4. close'):
+def preprocess(data):
+    data = data.rename(index=str, columns={x: x[3:].capitalize() for x in
+                                           data.columns})
+    data['Volume'] = np.log(data['Volume'])
+    data.index = range(len(data.index))
+    for indicator in indicators:
+        data = indicator(data)
+    data = data.shift(-WINDOW_SIZE)[:-WINDOW_SIZE]
+    return data
+
+
+def split_all_data_and_save(window=WINDOW_SIZE):
     all_inputs = []
-    all_deltas = []
+    all_labels = []
     for sym in os.listdir(RAW_DATA_DIR):
         if sym[0] != '.':
             data = load_sym(sym)
             if 'data' in data:
-                prices = data['data'][label].as_matrix()
-                if len(prices) > 2 * window:
-                    print 'Loading data from symbol %s...' % sym
-                    inputs, deltas = split(prices, window=window)
-                    assert (np.std(inputs) != 0).all()
-                    assert not np.isnan(inputs).any()
+                inputs = preprocess(data['data'])
+                if inputs.shape[0] > 2 * window:
+                    print('Loading data from symbol %s...' % sym)
+                    inputs, labels = split(inputs, window=window)
                     all_inputs.extend(inputs)
-                    all_deltas.append(deltas)
+                    all_labels.append(labels)
     all_inputs = np.concatenate(all_inputs).reshape(-1, window)
-    all_deltas = np.concatenate(all_deltas).reshape(-1, 1)
-    assert all_inputs.shape[0] == all_deltas.shape[0]
+    all_labels = np.concatenate(all_labels).reshape(-1, 1)
+    assert all_inputs.shape[0] == all_labels.shape[0]
     assert all_inputs.shape[1] == window
-    means = np.mean(all_inputs, axis=1, keepdims=True)
-    stds = np.std(all_inputs, axis=1, keepdims=True)
-    assert means.shape[0] == all_inputs.shape[0]
-    assert stds.shape[0] == all_inputs.shape[0]
-    all_inputs = np.divide((all_inputs - means), stds,
-                           out=np.zeros_like(all_inputs), where=stds!=0)
-    with open(os.path.join(TRAIN_DATA_DIR, 'prices'), 'w') as f:
+    with open(os.path.join(TRAIN_DATA_DIR, 'all_inputs.pkl'), 'wb') as f:
         pickle.dump(all_inputs, f)
-    with open(os.path.join(TRAIN_DATA_DIR, 'deltas'), 'w') as f:
-        pickle.dump(all_deltas, f)
-    return all_inputs, all_deltas
+    with open(os.path.join(TRAIN_DATA_DIR, 'all_labels.pkl'), 'wb') as f:
+        pickle.dump(all_labels, f)
+    return all_inputs, all_labels
 
 
-def output_to_txt(array, path):
-    with open(path, 'w') as f:
-        for arr in array:
-            f.write(' '.join(map(str, arr)) + '\n')
-
-def split_train_dev_test_to_file():
-    with open(os.path.join(TRAIN_DATA_DIR, 'prices')) as f:
-        prices = pickle.load(f)
-    with open(os.path.join(TRAIN_DATA_DIR, 'deltas')) as f:
-        deltas = pickle.load(f)
-    n = prices.shape[0]
-    print prices.shape
-    print deltas.shape
+def split_train_dev_test_to_file(inputs=None, labels=None):
+    if inputs is None:
+        with open(os.path.join(TRAIN_DATA_DIR, 'all_inputs.pkl')) as f:
+            inputs = pickle.load(f)
+    if labels is None:
+        with open(os.path.join(TRAIN_DATA_DIR, 'all_labels.pkl')) as f:
+            labels = pickle.load(f)
+    n = inputs.shape[0]
+    print(inputs.shape)
+    print(labels.shape)
     indices = np.random.permutation(n)
     train_idx, eval_idx, test_idx = \
         indices[:int(0.9*n)], indices[int(0.9*n):int(0.95*n)], \
         indices[int(0.95*n):]
-    output_to_txt(prices[train_idx, :],
-                  os.path.join(TRAIN_DATA_DIR, 'train_prices.txt'))
-    output_to_txt(prices[eval_idx, :],
-                  os.path.join(TRAIN_DATA_DIR, 'eval_prices.txt'))
-    output_to_txt(prices[test_idx, :],
-                  os.path.join(TRAIN_DATA_DIR, 'test_prices.txt'))
-    output_to_txt(deltas[train_idx, :],
-                  os.path.join(TRAIN_DATA_DIR, 'train_deltas.txt'))
-    output_to_txt(deltas[eval_idx, :],
-                  os.path.join(TRAIN_DATA_DIR, 'eval_deltas.txt'))
-    output_to_txt(deltas[test_idx, :],
-                  os.path.join(TRAIN_DATA_DIR, 'test_deltas.txt'))
+    pickle.dump(inputs[train_idx, :],
+                open(os.path.join(TRAIN_DATA_DIR, 'train_inputs.pkl'), 'rb'))
+    pickle.dump(inputs[eval_idx, :],
+                open(os.path.join(TRAIN_DATA_DIR, 'eval_inputs.pkl'), 'rb'))
+    pickle.dump(inputs[test_idx, :],
+                open(os.path.join(TRAIN_DATA_DIR, 'test_inputs.pkl'), 'rb'))
+    pickle.dump(labels[train_idx, :],
+                open(os.path.join(TRAIN_DATA_DIR, 'train_labels.pkl'), 'rb'))
+    pickle.dump(labels[eval_idx, :],
+                open(os.path.join(TRAIN_DATA_DIR, 'eval_labels.txt'), 'rb'))
+    pickle.dump(labels[test_idx, :],
+                open(os.path.join(TRAIN_DATA_DIR, 'test_labels.pkl'), 'rb'))
 
 
 def get_sym_and_save(sym):
     if os.path.exists(os.path.join(RAW_DATA_DIR, sym)):
-        print 'Symbol %s already downloaded. Skipping...' % sym
+        print('Symbol %s already downloaded. Skipping...' % sym)
         return
     try:
         data, metadata = TS.get_daily(symbol=sym, outputsize='full')
     except ValueError:
-        print 'Symbol %s does not exist! Skipping...' % sym
+        print('Symbol %s does not exist! Skipping...' % sym)
         with open(os.path.join(RAW_DATA_DIR, sym), 'w') as f:
             pickle.dump({}, f)
         return
@@ -124,7 +176,7 @@ def get_sym_and_save(sym):
 def get_all_raw_data(interval='1min'):
     syms = get_syms()
     for i, sym in enumerate(syms):
-        print '(%d/%d) Getting data for %s...' % (i, len(syms), sym)
+        print('(%d/%d) Getting data for %s...' % (i, len(syms), sym))
         get_sym_and_save(sym)
 
 
