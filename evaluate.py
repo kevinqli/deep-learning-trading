bb775417cebc126e52b2889017128421a47315d9
Kevin Q Li <likevinq@gmail.com>
2018-03-22 17:04:46 -0700

    new test script

--- evaluate.py
+++ evaluate.py
@@ -6,7 +6,7 @@ import os
 
 import tensorflow as tf
 
-from model.input_fn import input_fn
+from model.input_fn import input_fn, load_prices_and_deltas
 from model.model_fn import model_fn
 from model.evaluation import evaluate
 from model.utils import Params
@@ -16,7 +16,7 @@ from model.utils import set_logger
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='experiments/test',
                     help="Experiment directory containing params.json")
-parser.add_argument('--data_dir', default='data/64x64_SIGNS',
+parser.add_argument('--data_dir', default='data/',
                     help="Directory containing the dataset")
 parser.add_argument('--restore_from', default='best_weights',
                     help="Subdirectory of model dir or file containing the weights")
@@ -35,26 +35,22 @@ if __name__ == '__main__':
     # Set the logger
     set_logger(os.path.join(args.model_dir, 'evaluate.log'))
 
+    # Get paths for dataset
+    path_test_prices = os.path.join(args.data_dir, 'test_inputs.pkl')
+    path_test_deltas = os.path.join(args.data_dir, 'test_labels.pkl')
+
     # Create the input data pipeline
     logging.info("Creating the dataset...")
     data_dir = args.data_dir
-    test_data_dir = os.path.join(data_dir, "test_signs")
+    test_prices, test_deltas = load_prices_and_deltas(path_test_prices, path_test_deltas, params)
 
-    # Get the filenames from the test set
-    test_filenames = os.listdir(test_data_dir)
-    test_filenames = [os.path.join(test_data_dir, f) for f in test_filenames if f.endswith('.jpg')]
-
-    test_labels = [int(f.split('/')[-1][0]) for f in test_filenames]
-
-    # specify the size of the evaluation set
-    params.eval_size = len(test_filenames)
-
-    # create the iterator over the dataset
-    test_inputs = input_fn(False, test_filenames, test_labels, params)
+    # Create the two iterators over the two datasets
+    test_inputs = input_fn('test', test_prices, test_deltas, params)
+    logging.info("- done.")
 
     # Define the model
     logging.info("Creating the model...")
-    model_spec = model_fn('eval', test_inputs, params, reuse=False)
+    model_spec = model_fn('test', test_inputs, params, reuse=False)
 
     logging.info("Starting evaluation")
     evaluate(model_spec, args.model_dir, params, args.restore_from)
